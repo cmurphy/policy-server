164a9a9ccf474ccb7fa0d25cdd6ed7729bd1292d
Flavio Castelli <fcastelli@suse.com>
2020-12-15 17:58:07 +0100

    Use an unmanaged pool

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,9 @@ use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 use std::fs::File;
 use std::io::prelude::*;
 
-
-mod wapc_pool;
-use wapc_pool::WapcManager;
-
+use deadpool::unmanaged::Pool;
+use wapc::WapcHost;
+use wasmtime_provider::WasmtimeEngineProvider;
 
 fn load_file(path: &str) -> Vec<u8> {
     println!("{}", path);
@@ -16,22 +15,45 @@ fn load_file(path: &str) -> Vec<u8> {
     buf
 }
 
-#[get("/")]
-async fn hello(pool: web::Data<WapcManager>) -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+fn host_callback(
+    id: u64,
+    bd: &str,
+    ns: &str,
+    op: &str,
+    payload: &[u8],
+) -> Result<Vec<u8>, Box<dyn std::error::Error + Send + Sync>> {
+    println!(
+        "Guest {} invoked '{}->{}:{}' with payload of {}",
+        id,
+        bd,
+        ns,
+        op,
+        ::std::str::from_utf8(payload).unwrap()
+    );
+    Ok(b"Host result".to_vec())
 }
 
-type Pool = deadpool::managed::Pool<WapcManager, Box<dyn std::error::Error>>;
 
+#[get("/")]
+async fn hello(pool: web::Data<Pool<wapc::WapcHost>>) -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let module_bytes = load_file("test.wasm");
-    let mgr = WapcManager{module_bytes: module_bytes};
-    let pool = Pool::new(mgr, 10);
+
+    let pool = Pool::<wapc::WapcHost>::new(10);
+    for _ in 0..10 {
+        let engine = WasmtimeEngineProvider::new(&module_bytes, None);
+        let host = WapcHost::new(Box::new(engine), host_callback).expect("Failed to init waPC host");
+
+        pool.add(host);
+    }
 
     HttpServer::new(move || {
-        App::new::data(pool.clone())
+        App::new()
+            .data(pool)
             .service(hello)
     })
     .bind("127.0.0.1:8080")?
