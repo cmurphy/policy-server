1adde319c1d9971650f93ecd59f4cefcf9d665e1
Flavio Castelli <fcastelli@suse.com>
2020-12-16 17:43:51 +0100

    channel take

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use std::{convert::Infallible, net::SocketAddr};
+use hyper::{Body, Request, Response, Server};
+use hyper::service::{make_service_fn, service_fn};
+
+use tokio::sync::mpsc;
 
 use std::fs::File;
 use std::io::prelude::*;
 
-use deadpool::unmanaged::Pool;
 use wapc::WapcHost;
 use wasmtime_provider::WasmtimeEngineProvider;
 
@@ -34,29 +37,45 @@ fn host_callback(
 }
 
 
-#[get("/")]
-async fn hello(pool: web::Data<Pool<wapc::WapcHost>>) -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+fn init_host() -> WapcHost {
+    let module_bytes = load_file("test.wasm");
+    let engine = WasmtimeEngineProvider::new(&module_bytes, None);
+    WapcHost::new(Box::new(engine), host_callback).expect("Failed to init waPC host")
 }
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    let module_bytes = load_file("test.wasm");
+async fn handle(req: Request<Body>, mut tx: mpsc::Sender<String>) -> Result<Response<Body>, Infallible> {
+    tx.send(String::from("ciao")).await.unwrap();
+    Ok(Response::new("Hello, World!".into()))
+}
 
-    let pool = Pool::<wapc::WapcHost>::new(10);
-    for _ in 0..10 {
-        let engine = WasmtimeEngineProvider::new(&module_bytes, None);
-        let host = WapcHost::new(Box::new(engine), host_callback).expect("Failed to init waPC host");
+#[tokio::main]
+async fn main() {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
-        pool.add(host);
+    //let wapc_host = init_host();
+
+    let (tx, mut rx) = mpsc::channel(32);
+
+    let wasm_task = tokio::spawn(async move {
+        while let Some(cmd) = rx.recv().await {
+            println!("I received {}", cmd);
+            return;
+        }
+    });
+
+    let make_svc = make_service_fn(|_conn| {
+        let svc_tx = tx.clone();
+        async move {
+            Ok::<_, Infallible>(service_fn(move |req| {
+                handle(req, svc_tx.clone())
+            }))
+        }
+    });
+
+    let server = Server::bind(&addr).serve(make_svc);
+
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
     }
-
-    HttpServer::new(move || {
-        App::new()
-            .data(pool)
-            .service(hello)
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+    wasm_task.await.unwrap();
 }
